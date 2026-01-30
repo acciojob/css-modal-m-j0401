83a1a2f50e66e1d56c68b64d859095d2f652216d
m-j0401 <mehul.jain2206@gmail.com>
2026-01-30 11:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let d_one=document.querySelector(".modal");
+let btn=document.querySelector("#openModal");
+let c_btn=document.querySelector(".close-modal")
+btn.addEventListener("click",()=>{
+	d_one.style.display="block";
+	document.body.style.backgroundColor="darkgray";
+});
+c_btn.addEventListener("click",()=>{
+	d_one.style.display="none";
+	document.body.style.backgroundColor="white";
+})
